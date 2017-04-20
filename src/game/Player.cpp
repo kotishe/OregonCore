120e5fb9bf5689bfc5b19d6c5fe80f1868808b46
kotishe <soleks@gmail.com>
2017-04-21 00:27:49 +0300

    Just learn main prof, no levelup skill

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -659,11 +659,8 @@ bool Player::Create(uint32 guidlow, const std::string& name, uint8 race, uint8 c
     }
 
     LearnSpell(27028);
-    SetSkill(129,375,375);
     LearnSpell(33359);
-    SetSkill(185,375,375);
     LearnSpell(33095);
-    SetSkill(356,375,375);
 
     for (PlayerCreateInfoItems::const_iterator item_id_itr = info->item.begin(); item_id_itr != info->item.end(); ++item_id_itr)
         StoreNewItemInBestSlots(item_id_itr->item_id, item_id_itr->item_amount);
