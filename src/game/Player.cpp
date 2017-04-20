9ae79f5cb5946a48dc57a95997787c2b423061c9
kotishe <soleks@gmail.com>
2017-04-21 00:34:43 +0300

    Return original items

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -639,6 +639,11 @@ bool Player::Create(uint32 guidlow, const std::string& name, uint8 race, uint8 c
     // original spells
     LearnDefaultSpells(true);
 
+    // main profession
+    LearnSpell(27028);
+    LearnSpell(33359);
+    LearnSpell(33095);
+
 	UpdateSkillsToMaxSkillsForLevel();
 
     // original action bar
@@ -658,9 +663,55 @@ bool Player::Create(uint32 guidlow, const std::string& name, uint8 race, uint8 c
             ++action_itr[i];
     }
 
-    LearnSpell(27028);
-    LearnSpell(33359);
-    LearnSpell(33095);
+    // original items
+    CharStartOutfitEntry const* oEntry = NULL;
+    for (uint32 i = 1; i < sCharStartOutfitStore.GetNumRows(); ++i)
+    {
+        if (CharStartOutfitEntry const* entry = sCharStartOutfitStore.LookupEntry(i))
+        {
+            if (entry->RaceClassGender == RaceClassGender)
+            {
+                oEntry = entry;
+                break;
+            }
+        }
+    }
+
+    if (oEntry)
+    {
+        for (int j = 0; j < MAX_OUTFIT_ITEMS; ++j)
+        {
+            if (oEntry->ItemId[j] <= 0)
+                continue;
+
+            uint32 item_id = oEntry->ItemId[j];
+
+            ItemTemplate const* iProto = sObjectMgr.GetItemTemplate(item_id);
+            if (!iProto)
+            {
+                sLog.outErrorDb("Initial item id %u (race %u class %u) from CharStartOutfit.dbc not listed in item_template, ignoring.", item_id, getRace(), getClass());
+                continue;
+            }
+
+            uint32 count = iProto->Stackable;               // max stack by default (mostly 1)
+            if (iProto->Class == ITEM_CLASS_CONSUMABLE && iProto->SubClass == ITEM_SUBCLASS_FOOD)
+            {
+                switch (iProto->Spells[0].SpellCategory)
+                {
+                case SPELL_CATEGORY_FOOD:                                // food
+                    if (iProto->Stackable > 4)
+                        count = 4;
+                    break;
+                case SPELL_CATEGORY_DRINK:                                // drink
+                    if (iProto->Stackable > 2)
+                        count = 2;
+                    break;
+                }
+            }
+
+            StoreNewItemInBestSlots(item_id, count);
+        }
+    }
 
     for (PlayerCreateInfoItems::const_iterator item_id_itr = info->item.begin(); item_id_itr != info->item.end(); ++item_id_itr)
         StoreNewItemInBestSlots(item_id_itr->item_id, item_id_itr->item_amount);
