8af8cbdf717f4431540382605b1e9ee2c09ecf1c
Ghaster <defscam@gmail.com>
2017-08-13 12:53:39 +0200

    Fix environmental damage on resurrection
    Source:
    https://github.com/elysium-project/server/commit/b0df4d7611c347122b43b131a1e1534c7c196c3a

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -20200,9 +20200,9 @@ void Player::ResurectUsingRequestData()
     /// Teleport before resurrecting by player, otherwise the player might get attacked from creatures near his corpse
     TeleportTo(m_resurrectMap, m_resurrectX, m_resurrectY, m_resurrectZ, GetOrientation());
 
-    //we cannot resurrect player when we triggered far teleport
-    //player will be resurrected upon teleportation
-    if (IsBeingTeleportedFar())
+    //we cannot resurrect player when we triggered any kind of teleport
+    //player will be resurrected upon teleportation (in MSG_MOVE_TELEPORT_ACK handler)
+    if (IsBeingTeleported())
     {
         ScheduleDelayedOperation(DELAYED_RESURRECT_PLAYER);
         return;
